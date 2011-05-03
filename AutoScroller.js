bbf4e43a51d1734da6006aa9627dcef4c08b02de
Patrick Talmadge <ptalmadge@gmail.com>
2011-05-03 08:08:32 -0700

    add files

---
+++ AutoScroller.js
@@ -0,0 +1,55 @@
+var AutoScroller = new function () {
+    this.speed = 1;
+    this.div = "NOSET";
+
+    //These should be private
+    var accel = this.speed;
+    var down = true;
+    var pause = false;
+
+    this.scroll = function () {
+        if (this.div == "NOSET") {
+            alert('Set div to scroll');
+            return;
+        }
+        this.div.scrollTop = this.div.scrollTop + accel;
+
+        var timer = 100;
+        //If scroller hits top or bottom change scroll direction
+        if (this.div.scrollTop + accel <= 0 || this.div.scrollTop + accel >= (this.div.scrollHeight - this.div.clientHeight)) {
+            this.changeDirection();
+            timer = 1500;
+        }
+
+        if (pause == false) {
+            setTimeout("AutoScroller.scroll()", timer);
+        }
+    };
+
+    this.scrollDown = function () {
+        this.div.scrollTop = this.div.scrollTop + 10;
+    };
+
+    this.scrollUp = function () {
+        this.div.scrollTop = this.div.scrollTop - 10;
+    };
+
+    this.changeDirection = function () {
+        down = !down;
+        if (down) {
+            accel = this.speed;
+        }
+        else {
+            accel = -this.speed;
+        }
+    };
+
+    this.pauseScroller = function () {
+        pause = true;
+    };
+
+    this.startScroller = function () {
+        pause = false;
+        this.scroll();
+    };
+} ();
